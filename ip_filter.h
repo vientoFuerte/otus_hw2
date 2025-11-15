3accd374bfa727cdf3e85a52a290bcd8a1ce0ec5
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2025-11-15 19:42:34 +0300

    Update ip_filter.h
    Убран повтор.

--- ip_filter.h
+++ ip_filter.h
@@ -2,18 +2,15 @@
 #include <stdint.h>
 #include <string>
 
-
 struct IPAddress {
      uint32_t ip;
 };
 
-
-
-
 int32_t ipStrToInt(const std::string& ipStr);
+
 std::string intToIPStr(uint32_t ip);
+
 bool compareIP(const IPAddress& a, const IPAddress& b);
-uint8_t getFirstByte(uint32_t ip);
 
 // Функция для получения первого байта IP адреса
 uint8_t getFirstByte(uint32_t ip);
@@ -23,7 +20,6 @@ uint8_t getSecondByte(uint32_t ip);
 
 // Функция для получения третьего байта IP адреса
 uint8_t getThirdByte(uint32_t ip);
-
 
 // Функция для получения четвертого байта IP адреса
 uint8_t getFourthByte(uint32_t ip);
