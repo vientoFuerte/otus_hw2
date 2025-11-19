6140901c86c38462ebc580c8683b34f3084ae0e5
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2025-11-19 21:39:29 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,7 @@
 // main.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 
 #include <cassert>
-#include <iostream>
-#include <string>
-#include <vector>
-#include <fstream>
-#include <algorithm>
-#include <sstream>
-
-#include "ip_filter.cpp"
+#include "ip_filter.h"
 
 
 int main()
@@ -20,24 +13,6 @@ int main()
 
     std::ifstream inputFile;
 
-#if 0
-    std::string filename = "ip_filter.tsv";
-
-    // Открываем файл для чтения
-    std::ifstream inputFile(filename);
-
-    if (!inputFile.is_open()) 
-    {
-        std::cerr << "File open error: " << filename << std::endl;
-        std::cerr << "Error code: " << inputFile.rdstate() << std::endl;
-        return 1;
-    }
-    else
-    {
-        std::cout << "file opened" << std::endl;
-    }
-
-#endif
     try
     {
         // Читаем IP-адреса из файла
@@ -47,11 +22,7 @@ int main()
         {
             ip_pool.push_back(parseIPAddress(line));
         }
-        /*
-        while (std::getline(inputFile, line)) 
-        {
-            ip_pool.push_back(parseIPAddress(line));
-        }*/
+
 
         // Сортируем IP-адреса в числовом представлении
         std::sort(ip_pool.begin(), ip_pool.end(), compareIP);
@@ -64,7 +35,7 @@ int main()
         // Фильтрация IP адресов по первому байту
         std::vector<IPAddress> filteredIPs;
         for (const auto& ipObj : ip_pool) {
-            if (getFirstByte(ipObj.ip) == 1) {
+            if (getByteFromIP(ipObj.ip, 0) == 1) {
                 filteredIPs.push_back(ipObj);
             }
         }
@@ -78,8 +49,11 @@ int main()
         filteredIPs.clear();
 
         // Фильтрация IP адресов по первому и второму байтам
-        for (const auto& ipObj : ip_pool) {
-            if (getFirstByte(ipObj.ip) == 46 && getSecondByte(ipObj.ip) == 70) {
+        for (const auto& ipObj : ip_pool) 
+        {
+            if ((getByteFromIP(ipObj.ip, 0) == 46) && 
+                (getByteFromIP(ipObj.ip, 1) == 70)) 
+            {
                 filteredIPs.push_back(ipObj);
             }
         }
@@ -94,10 +68,10 @@ int main()
 
         // Фильтрация IP адресов по любому байту
         for (const auto& ipObj : ip_pool) {
-            if (getFirstByte(ipObj.ip) == 46 || 
-                (getSecondByte(ipObj.ip) == 46) ||
-                (getThirdByte(ipObj.ip) == 46) ||
-                (getFourthByte(ipObj.ip) == 46)) 
+            if (getByteFromIP(ipObj.ip, 0) == 46 ||
+                (getByteFromIP(ipObj.ip, 1) == 46) ||
+                (getByteFromIP(ipObj.ip, 2) == 46) ||
+                (getByteFromIP(ipObj.ip, 3) == 46))
             {
                 filteredIPs.push_back(ipObj);
             }
@@ -108,79 +82,15 @@ int main()
             std::cout << intToIPStr(ipObj.ip) << std::endl;
         }
 
-
-        // 222.173.235.246
-        // 222.130.177.64
-        // 222.82.198.61
-        // ...
-        // 1.70.44.170
-        // 1.29.168.152
-        // 1.1.234.8
-
-        // TODO filter by first byte and output
-        // ip = filter(1)
-
-        // 1.231.69.33
-        // 1.87.203.225
-        // 1.70.44.170
-        // 1.29.168.152
-        // 1.1.234.8
-
-        // TODO filter by first and second bytes and output
-        // ip = filter(46, 70)
-
-        // 46.70.225.39
-        // 46.70.147.26
-        // 46.70.113.73
-        // 46.70.29.76
-
-        // TODO filter by any byte and output
-        // ip = filter_any(46)
-
-        // 186.204.34.46
-        // 186.46.222.194
-        // 185.46.87.231
-        // 185.46.86.132
-        // 185.46.86.131
-        // 185.46.86.131
-        // 185.46.86.22
-        // 185.46.85.204
-        // 185.46.85.78
-        // 68.46.218.208
-        // 46.251.197.23
-        // 46.223.254.56
-        // 46.223.254.56
-        // 46.182.19.219
-        // 46.161.63.66
-        // 46.161.61.51
-        // 46.161.60.92
-        // 46.161.60.35
-        // 46.161.58.202
-        // 46.161.56.241
-        // 46.161.56.203
-        // 46.161.56.174
-        // 46.161.56.106
-        // 46.161.56.106
-        // 46.101.163.119
-        // 46.101.127.145
-        // 46.70.225.39
-        // 46.70.147.26
-        // 46.70.113.73
-        // 46.70.29.76
-        // 46.55.46.98
-        // 46.49.43.85
-        // 39.46.86.85
-        // 5.189.203.46
+ 
     }
     catch (const std::exception & e)
     {
         std::cerr << e.what() << std::endl;
     }
 
-    // Закрываем файл
-    //inputFile.close();
 
     return 0;
 }
 
-
+s
