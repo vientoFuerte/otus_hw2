22ebdcaad73732fd48dcd3024e7e76312c3bb73f
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2025-11-23 17:07:36 +0300

    Delete build directory

--- build/CMakeFiles/ip_filter_lib.dir/lib.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/ip_filter_lib.dir/lib.cpp.o: \
- /home/viento/otus/otus_hw2/lib.cpp /usr/include/stdc-predef.h \
- /home/viento/otus/otus_hw2/lib.h \
- /home/viento/otus/otus_hw2/build/version.h
