45f8e99d9f12b7edf801a0fd1d7223259e058c9d
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2025-11-24 12:18:43 +0300

    Update ip_filter.h

--- ip_filter.h
+++ ip_filter.h
@@ -31,5 +31,51 @@ IPAddress parseIPAddress(const std::string& ipStr);
 // Фильтрация IP адресов по любому байту
 std::vector<IPAddress> filterByAnyByte(const std::vector<IPAddress>& ipPool, uint8_t byte);
 
+template <typename T>
+void addBytes(std::vector<uint8_t>& vec, T byte) {
+    vec.push_back(static_cast<uint8_t>(byte));
+}
+
+template <typename T, typename... Args>
+void addBytes(std::vector<uint8_t>& vec, T first, Args... rest) {
+    vec.push_back(static_cast<uint8_t>(first));
+    addBytes(vec, rest...);
+}
+
+template <typename... Args>
+std::vector<uint8_t> addBytesToVector(Args... bytes) {
+    std::vector<uint8_t> result;
+    addBytes(result, bytes...);
+    return result;
+}
+
+// Рекурсивная функция фильтрации с variadic templates
+// Для шаблонных функций реализация в заголовке!
+template <typename T, typename... Args>
+std::vector<IPAddress> FilterIpPool(const std::vector<IPAddress>& ipPool, T firstByte, Args... otherBytes) {
+    std::vector<IPAddress> result;
+    // Сложили в вектор входные байты для проверки на соответствие
+    std::vector<uint8_t> filterBytes = addBytesToVector(firstByte, otherBytes...);
+    
+    // Фильтруем IP-адреса
+    for (const auto& ipAddr : ipPool) {
+        bool matches = true;
+        
+        // Проверяем соответствие каждому байту фильтра
+        for (size_t i = 0; i < filterBytes.size(); ++i) {
+            if (getByteFromIP(ipAddr.ip, i) != filterBytes[i]) {
+                matches = false;
+                break;
+            }
+        }
+        
+        if (matches) {
+            result.push_back(ipAddr);
+        }
+    }
+    
+    return result;
+}
+
 // Вывести на печать все адреса из списка
 void printIPAddresses(const std::vector<IPAddress>& ipObjects);
